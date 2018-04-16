82431cfb58cabaca91313f9ad7f16f27fbb773a8
Ming-Tang <project.shinkirou@gmail.com>
2018-04-16 15:49:33 -0700

    Implement REPL

--- Main.hs
+++ Main.hs
@@ -1,10 +1,34 @@
 module Main where
+import Data.List(isPrefixOf)
+import Control.Exception
+import Control.Monad.IO.Class
+import System.Console.Repline
+
 import Types
 import Infer
 import Parser
 import FType
 import Convert
 import PrettyExpr
+
+type Repl a = HaskelineT IO a
+
+f1 g xs = liftIO ((print $ g $ unwords xs) `catch` (\e -> print (e :: ErrorCall)))
+
+options :: [(String, [String] -> Repl ())]
+options = [
+    ("t", f1 typeFromExpr)
+  , ("tp", f1 typeFromPExpr)
+  , ("ce", f1 convExpr)
+  , ("prop", f1 prop_convExpr)
+  ]
+
+cmd :: String -> Repl ()
+cmd input = liftIO $ print input
+
+-- Tab Completion: return a completion for partial words entered
+completer :: Monad m => WordCompleter m
+completer n = return $ filter (isPrefixOf n) $ map fst options
 
 testType = () :< ECompose (() :< ESym (() :< EIso ZeroE))
                           (() :< EIso SwapS)
@@ -25,10 +49,14 @@ typeTreePExpr s = typeTree $ parsePExpr s
 
 convExpr s = joinEither $ (prettyExpr <$>) . convertExpr <$> typeTree (parseExpr s)
 
-prop_convExpr e = t == t' where
-  t = either (const Nothing) Just $ joinEither $ typeFromPExpr <$> convExpr e
-  t' = either (const Nothing) Just $ joinEither $ typeFromPExpr <$> convExpr e
+prop_convExpr e = t `seq` t' `seq` t == t' where
+  t = toM $ typeFromExpr e
+  t' = toM $ joinEither $ typeFromPExpr <$> convExpr e
+  toM = either (const Nothing) Just
+
+ini :: Repl ()
+ini = liftIO $ return ()
 
 main :: IO ()
-main = print $ typeTree testType
+main = evalRepl ">>> " cmd options (Word completer) ini
 
